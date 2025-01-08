282800412cfdf2585a301892308d376ae21b393c
albiagro <alberto.agro@live.it>
2025-01-08 23:36:20 +0100

    Redirecting

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var usersController = require('./controllers/usersController');
 const env = require("dotenv");
+const path = require('path');
 const partiesController = require('./controllers/partiesController');
 env.config();
 
-var app = express();
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //static files
 app.use(express.static('./public'));
