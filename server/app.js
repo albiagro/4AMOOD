c71f26e451de64bdf7eedd6b35bf379de1c1f788
Alberto Agrò <134003520+albiagro@users.noreply.github.com>
2025-01-22 00:33:59 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.static('./public'));
 
 app.use(function(req, res, next) {
     // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ['http://localhost:3000', 'https://fouramood.netlify.app'];
+    const allowedOrigins = ['http://localhost:3000', 'https://fouramood.netlify.app', 'https://fouramood-mgqn.onrender.com'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
          res.setHeader('Access-Control-Allow-Origin', origin);
