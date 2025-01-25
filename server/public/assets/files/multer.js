4b5cfed3740c6ae408ab46a4746d8ee969d10e83
albiagro <alberto.agro@live.it>
2025-01-25 16:08:28 +0100

    Fixing missing path declaration

--- server/public/assets/files/multer.js
+++ server/public/assets/files/multer.js
@@ -1,6 +1,7 @@
 const aws = require('aws-sdk');
 const multerS3 = require('multer-s3');
 const multer = require('multer');
+const path = require('path');
 
 const s3 = new aws.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
